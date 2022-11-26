5fd3bc507ee1280bbad57d643e2d542c2a74d7dc
Shoukathsandy <shoukathsandy@gmail.com>
2022-11-26 18:18:03 +0530

    heroku

--- src/redux/api.js
+++ src/redux/api.js
@@ -2,12 +2,13 @@ import axios from "axios";
 
 // const devEnv = process.env.NODE_ENV !== "production";
 
-const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
+// const { REACT_APP_DEV_API, REACT_APP_PROD_API } = process.env;
 
 const API = axios.create({
   // baseURL: `${devEnv ? REACT_APP_DEV_API : REACT_APP_PROD_API}`,
-  baseURL: "http://localhost:5000",
-
+  baseURL: "https://tour-pedia-app.herokuapp.com",
+  
+  // http://localhost:5000
 });
 
 API.interceptors.request.use((req) => {
